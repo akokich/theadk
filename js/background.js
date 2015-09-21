6fce250505300459c2d70617884e2d5b708a8765
akokich <aaron@theadk.com>
2015-09-21 14:41:49 -0700

    Initial Check In
    Initial check in of the adk website

---
+++ js/background.js
@@ -0,0 +1,113 @@
+﻿window.addEventListener('DOMContentLoaded', function () {
+    var canvas = document.getElementById("renderCanvas");
+    var engine = new BABYLON.Engine(canvas, true);
+
+    var createScene = function () {
+        var scene = new BABYLON.Scene(engine);
+
+        scene.clearColor = new BABYLON.Color3(0.0, 0.0, 0.0);
+
+        var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 0, -15), scene);
+        camera.setTarget(BABYLON.Vector3.Zero());
+        camera.attachControl(canvas, false);
+
+        var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 0, -10), scene);
+        light.diffuse = new BABYLON.Color3(0, 0, 0);
+        light.specular = new BABYLON.Color3(0, 0.3, 0.4);
+
+        var light2 = new BABYLON.HemisphericLight('light2', new BABYLON.Vector3(0, -10, 0), scene);
+        light2.diffuse = new BABYLON.Color3(0, 0, 0);
+        light2.specular = new BABYLON.Color3(0, 0.3, 0.4);
+
+        var planeMaterial = generateMirrorMaterial(scene);
+
+        planes(scene, planeMaterial);
+
+        return scene;
+    }
+    var scene = createScene();
+
+    engine.runRenderLoop(function () {
+        var deltaTime = engine.getDeltaTime();
+
+        scene.render();
+    });
+
+    window.addEventListener('resize', function () {
+        engine.resize();
+    });
+});
+
+// Planes prototype
+function planes (scene, material)
+{
+    var animationDuration = 2000;
+    this.mesh = [
+        new BABYLON.Mesh.CreateBox("box01", 20.0, scene),
+        new BABYLON.Mesh.CreateBox("box02", 20.0, scene),
+        new BABYLON.Mesh.CreateBox("box03", 20.0, scene)
+    ];
+
+    mesh[0].position.x -= 15;
+    mesh[2].position.x += 15;
+
+    mesh[1].rotation.x += 42;
+
+    this.animation = [
+        new BABYLON.Animation("planeAni01", "rotation.y", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_RELATIVE),
+        new BABYLON.Animation("planeAni01", "rotation.x", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_RELATIVE),
+        new BABYLON.Animation("planeAni01", "rotation.y", -30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_RELATIVE)
+    ];
+
+    this.upAniKeys = [];
+    upAniKeys.push({
+        frame: 0,
+        value: 1
+    });
+
+    upAniKeys.push({
+        frame: animationDuration,
+        value: 3
+    });
+
+    this.downAniKeys = [];
+    downAniKeys.push({
+        frame: 0,
+        value: 0
+    });
+
+    downAniKeys.push({
+        frame: animationDuration,
+        value: -Math.PI
+    });
+
+    animation[0].setKeys(upAniKeys);
+    animation[1].setKeys(upAniKeys);
+    animation[2].setKeys(downAniKeys);
+
+    mesh[0].animations.push(animation[0]);
+    mesh[1].animations.push(animation[1]);
+    mesh[2].animations.push(animation[2]);
+
+    
+    scene.beginAnimation(mesh[0], 0, animationDuration, true);
+    scene.beginAnimation(mesh[1], 0, animationDuration, true);
+    scene.beginAnimation(mesh[2], 0, animationDuration, true);
+    /* */
+    mesh[0].material = material;
+    mesh[1].material = material;
+    mesh[2].material = material;
+
+    // move the middle out
+    mesh[1].position.z = 10;
+}
+
+function generateMirrorMaterial (scene)
+{
+    var mirrorMat = new BABYLON.StandardMaterial('mirror material', scene);
+    mirrorMat.alpha = 0.5;
+    mirrorMat.backFaceCulling = true;
+    mirrorMat.specularPower = 32;
+
+    return mirrorMat;
+}
---
+++ js/theadk.js
@@ -0,0 +1,77 @@
+﻿var highlightEnabled = true;
+
+$(document).ready(function () {
+    //$('#renderOverlay').hide();           // for background debug
+
+    $('#contentDiv').perfectScrollbar();
+
+    window.addEventListener('hashchange', router);
+    window.addEventListener('load', router);
+
+    $("#contentDiv").bind('scroll', function () {
+        if (highlightEnabled) {
+            var offset = $('#contentDiv').scrollTop();
+            if (offset > 400) {
+                $('#contentHighlight').hide();
+                $('#highlightPhrase').text(getPhrase());
+            } else {
+                $('#contentHighlight').show();
+            }
+        }
+    });
+
+    $('#header').load('html/header.html');
+    $('#footer').load('html/footer.html');
+    handleResize();
+});
+
+function handleResize() {
+    var size = $('#headerOverlay').height() + $('#contentHighlight').height();
+    $('#headerSpacer').height(size);
+
+    return size;
+}
+
+function getPhrase ()
+{
+    var phrases = [
+        "developer. creative. nerd.",
+        "general purpose dork.",
+        "full-stack developer.",
+        "C#, Java, SQL, JS, HTML5/CSS.",
+    ];
+
+    return phrases[Math.floor((Math.random() * phrases.length))];
+}
+
+function router (html)
+{
+    $('#contentDiv').fadeOut(300, "swing", function () {
+        var url = location.hash.slice(1) || '/';
+        
+        if (url == "/" || url == "home") {
+            $('#contentHighlight').show();
+            highlightEnabled = true;
+
+            $('#mainBody').load('html/home.html');
+
+            // change the phrase
+            $('#highlightPhrase').text(getPhrase());
+        } else {
+            $('#contentHighlight').hide();
+            highlightEnabled = false;
+
+            $('#mainBody').load('html/' + url + '.html', function (response, status, xhr) {
+                if (status == "error") {
+                    console.log("ERROR");
+                    $("#mainBody").html("<h1>Not found</h1><p>The page you request doesn't exist.</p>");
+                }
+            });
+        }
+
+        $('#contentDiv').fadeIn(150, "swing", function () {
+            $('#contentDiv').animate({ scrollTop: 0 }, 300);
+            //$('#contentDiv').scrollTop(0);
+        });
+    });
+}
